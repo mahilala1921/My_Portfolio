caf7a556258dcd9fa2a1ae6e179d99e138499c52
mahilala1921 <mahilalalex27@gmail.com>
2026-09-15 14:28:06 +0300

    fix email js

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,9 +3,9 @@ import emailjs from '@emailjs/browser'
 import styles from './Contact.module.css'
 import { Button } from '../ui/Button'
 
-const EMAILJS_SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID
+const EMAILJS_SERVICE_ID = 'service_q83wajl'
 const EMAILJS_TEMPLATE_ID = 'template_8fkox9i'
-const EMAILJS_PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+const EMAILJS_PUBLIC_KEY = 'oXOdHgAIQagzo1054'
 
 export function Contact() {
   const [name, setName] = useState('')
